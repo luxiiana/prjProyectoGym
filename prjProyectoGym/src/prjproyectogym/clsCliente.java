b0b1cf549ee24056c77bec7f602561c09ea32c0b
Sebastian Villalobos <dereklevilla45@gmail.com>
2022-08-03 20:06:24 -0600

    Update clsCliente.java

--- prjProyectoGym/src/prjproyectogym/clsCliente.java
+++ prjProyectoGym/src/prjproyectogym/clsCliente.java
@@ -6,10 +6,11 @@ public class clsCliente {
 
     ///---------Info Personal------
     private String nombreC;
-    private String origen;
     private String identificacion;
-    private String telefono;
     private String sexo;
+    private String edad;
+    private String origen;
+    private String telefono;
     private String tipoDePase;
     /*  
         Basico [] solo acceso a gym || no ucpa ningun calculo
@@ -35,23 +36,26 @@ public class clsCliente {
     //clsAlimentacion PlanAlimentacion;
     int contadorC = 0;
     clsCliente bdClientes[] = new clsCliente[9999999];
+    cls_funciones clsF = new cls_funciones();
 
-    public clsCliente(String nombreC, String origen, String identificacion, String telefono, String sexo, String somatotipo, String objetivo, String tipoDePase, float caloriasDiarias, float pago, float pesoKg, float estaturaCm, clsRutina rutina) {
+    public clsCliente(String nombreC, String identificacion, String sexo, String edad, String origen, String telefono, String tipoDePase, float pago, String somatotipo, String objetivo, float caloriasDiarias, float pesoKg, float estaturaCm, clsRutina rutina) {
         this.nombreC = nombreC;
-        this.origen = origen;
         this.identificacion = identificacion;
-        this.telefono = telefono;
         this.sexo = sexo;
+        this.edad = edad;
+        this.origen = origen;
+        this.telefono = telefono;
+        this.tipoDePase = tipoDePase;
+        this.pago = pago;
         this.somatotipo = somatotipo;
         this.objetivo = objetivo;
-        this.tipoDePase = tipoDePase;
         this.caloriasDiarias = caloriasDiarias;
-        this.pago = pago;
         this.pesoKg = pesoKg;
         this.estaturaCm = estaturaCm;
         this.rutina = rutina;
-
     }
+
+    
 
     public String getNombreC() {
         return nombreC;
@@ -157,21 +161,51 @@ public class clsCliente {
         this.sexo = sexo;
     }
 
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+    
+
     public void agregarCliente() { // Derek
-        String controlador = "";
-        do {
-            try {
+        
+        //--------------------------Variables--------------------------
+        String nombre="",identificacion="",sexo="", origen="",edadS="",telefono="",tipoDePase="",somatotipo="",objetivo="",caloriasDiariasS="",pesoKgS="",estaturaCmS="",pagoS="";
+        int edad=0;
+        float pago=0,caloriasDiarias=0,pesoKg=0,estaturaCm=0;
+        
+        try {
+            //-------Pide nombre del Cliente mientras sea menor que 5 caracteres--------
+            while (nombre.length() < 5 || nombre.equals("")) {
+                nombre = JOptionPane.showInputDialog("Digite el nombre completo del cliente");
+                if (nombre.length() < 5 || nombre.equals("")) {
+                    //Si el nombre que se introdujo es menor a 5 caracteres se le muestra el siguiente mensaje 
+                    JOptionPane.showMessageDialog(null, "! Nombre invalido ¡\nDigite un nombre valido");
+                }
+            }//fin pedir nombre de Cliente
+            
+            //--------------------Pedir edad del empleado--------------------------
+                while (!clsF.esNumero(edadS) || !(Integer.parseInt(edadS) >= 18 && Integer.parseInt(edadS) <= 100)) { //Se comprueba si es numero
+                    edadS = JOptionPane.showInputDialog("Digite su edad:");
+                    if (!clsF.esNumero(edadS) || !(Integer.parseInt(edadS) >= 18 && Integer.parseInt(edadS) <= 100)) {
+                        JOptionPane.showMessageDialog(null, "EDAD INVALIDA");
+                    }
+                }
+                edad = Integer.parseInt(edadS); //la edadS se convierta a numero
+                //------------------FIN edad-------------------------------------------
 
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, "Digi");
-            }
+            
+            //--------
+            bdClientes[contadorC]=new clsCliente(nombre, identificacion, sexo, edad, origen, telefono, tipoDePase, pago, somatotipo, objetivo, caloriasDiarias, pesoKg, estaturaCm, rutina);
+            //bdClientes[contadorC] = new clsEmpleados(nombreC, identificacion, origen, nombreC, telefono, sexo, contadorC, sexo);
+            contadorC++;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al Registrar el Cliente");
+        }
 
-        } while (!controlador.equals("Z"));
     }//fin agregar
-    
-    
-    
-    
-    
 
 }//fin clsClientes
